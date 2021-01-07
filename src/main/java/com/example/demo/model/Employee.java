b7018490e17effa45b2afe74ea9ed8580e8ab32b
VladislavMandrik <76885776+VladislavMandrik@users.noreply.github.com>
2021-01-07 17:07:04 +0300

    add lombok to Employee

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -15,15 +18,6 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String emailId;
-
-    public Employee() {
-    }
-
-    public Employee(String firstName, String lastName, String emailId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailId = emailId;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,25 +34,13 @@ public class Employee {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     @Column(name = "last_name", nullable = false)
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     @Column(name = "email_address", nullable = false)
     public String getEmailId() {
         return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
     }
 }
