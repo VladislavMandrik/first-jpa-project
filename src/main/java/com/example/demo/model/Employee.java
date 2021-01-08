1c53a04802d0998ac27a456f32fc813a36521cf4
VladislavMandrik <76885776+VladislavMandrik@users.noreply.github.com>
2021-01-08 23:47:27 +0300

    fix lombok to Employee

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -8,39 +8,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 @Data
 @Entity
 @Table(name = "employees")
 public class Employee {
 
-    private long id;
-    private String firstName;
-    private String lastName;
-    private String emailId;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
+    private Long id;
     @Column(name = "first_name", nullable = false)
-    public String getFirstName() {
-        return firstName;
-    }
-
+    private String firstName;
     @Column(name = "last_name", nullable = false)
-    public String getLastName() {
-        return lastName;
-    }
-
+    private String lastName;
     @Column(name = "email_address", nullable = false)
-    public String getEmailId() {
-        return emailId;
-    }
+    private String emailId;
 }
